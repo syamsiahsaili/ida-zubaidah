0eab3a04380337f9022f43fc2d41fcaa9be666ab
syamsiahsaili <syamsiahsaili64@gmail.com>
2021-10-17 20:25:41 -0700

    menambahkan file tugas 1

---
+++ pertemuan 2/Praktikum daspro 2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout<<"Nama : Siti Zubaidah"<<endl;
+	cout<<"NIM : F1G121011"<<endl;
+	cout<<"Tugas Praktikum Daspro 2"<<endl;
+	
+	//Mencari Rumu Volume Kubus
+	
+	
+	int sisi_a, sisi_b, sisi_c;
+	 
+	cout<<"masukkan nilai sisi_a = ";
+	cin>>sisi_a;
+	cout<<"masukkan niilai sisi_b = ";
+	cin>>sisi_b;
+	cout<<"masukkan nilai sisi_c = ";
+	cin>>sisi_c;
+	cout<<"jadi volume kubus adalah = "<<sisi_a * sisi_b * sisi_c;
+	cout<<"cm^3"<<endl;
+	
+	//mencari luas lingkaran
+	
+	float phi,r;
+	cout<<"masukkan nilai phi = ";
+	cin>>phi;
+	cout<<"masukkan nilai r = ";
+	cin>>r;
+	cout<<"masukkan nilai r = ";
+	cin>>r;
+	cout<<"jadi luas lingkaran adalah = " <<phi * r * r;
+	cout<<"cm^2"<<endl;
+	
+	return 0;
+	
+    
+	
+	
+}
